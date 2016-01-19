cdb755d2c1766533db09b149ebee73943c7a03ec
Jerry Tan <jerrytansk@gmail.com>
2016-01-19 11:12:02 +0800

    Facebook login, plus sdk installed. Geolocation gets user's current location. Tested on firefox macbook air.

--- client/init.js
+++ client/init.js
@@ -1,15 +1,44 @@
 Meteor.startup(function() {
+	// Initialize FB javascript SDK
+	window.fbAsyncInit = function() {
+    	FB.init({
+      		appId      : '446286968895184',
+      		status     : true,
+      		xfbml      : true
+    	});
+  	};
+  	FB.ui({
+  		method : 'feed'
+  	});
+	window.onload = function(){
+		var currentLocation;
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(function(position){
+				currentLocation = position;
+			}, function(error){
+				console.log('An error has occured while getting user\'s location' + error.code);
+				// error.code can be:
+	    		//   0: unknown error
+	    		//   1: permission denied
+	    		//   2: position unavailable (error response from locaton provider)
+	    		//   3: timed out
+			});
+		}
+	  	console.log(currentLocation);
+		$.getScript("https://api.tiles.mapbox.com/mapbox-gl-js/v0.12.3/mapbox-gl.js", function(){
+			Session.set("mapbox-gl", 1);
+			mapboxgl.accessToken = 'pk.eyJ1IjoiamVycnl0YW4iLCJhIjoiY2lqazVjdGJiMDMybXU0bHQ4a2kzOWI5biJ9.W57rFm6pWbNxfsagv_NX5Q';
+			var map = new mapboxgl.Map({
+			    container: 'map', // container id
+			    style: 'mapbox://styles/mapbox/emerald-v8', //stylesheet location
+			    center: [currentLocation.coords.longitude, currentLocation.coords.latitude], // starting position is user's location
+			    zoom: 14.5 // starting zoom
+			});	
+		})
+	}
+	Accounts.ui.config({
+    	passwordSignupFields : "USERNAME_ONLY"
+  	});
 	// Potentially prompts the user to enable location services. We do this early
 	// on in order to have the most accurate location by the time the user shares
-	$.getScript("https://api.tiles.mapbox.com/mapbox-gl-js/v0.12.3/mapbox-gl.js", function(){
-		console.log("hello world!");
-		Session.set("mapbox-gl", 1);
-		mapboxgl.accessToken = 'pk.eyJ1IjoiamVycnl0YW4iLCJhIjoiY2lqazVjdGJiMDMybXU0bHQ4a2kzOWI5biJ9.W57rFm6pWbNxfsagv_NX5Q';
-		var map = new mapboxgl.Map({
-		    container: 'map', // container id
-		    style: 'mapbox://styles/mapbox/emerald-v8', //stylesheet location
-		    center: [103.776, 1.296], // starting position
-		    zoom: 14.5 // starting zoom
-		});	
-	})
 });
---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+ 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '446286968895184',
+    secret: '8eded3185291501cfad56967d8fcb7b5'
+});
