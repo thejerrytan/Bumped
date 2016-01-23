06ec5e7c3125bed7275440f83206c4bf5be5921c
Zheng Yang Tean <teanzhengyang@outlook.sg>
2016-01-23 18:46:42 +0800

    1 duck

--- client/templates/landingpage.js
+++ client/templates/landingpage.js
@@ -23,17 +23,26 @@ Template.landingpage.helpers({
 
 Template.landingpage.onRendered(function(){
 	var map = null;
+
 	Tracker.autorun(function () {
 		if (Mapbox.loaded()) {
 			L.mapbox.accessToken = 'pk.eyJ1IjoiamVycnl0YW4iLCJhIjoiY2lqazVjdGJiMDMybXU0bHQ4a2kzOWI5biJ9.W57rFm6pWbNxfsagv_NX5Q';
 			map = L.mapbox.map("map", "mapbox.emerald");
-			getLocation(map);	
+			featureLayer = L.mapbox.featureLayer().addTo(map);
+			featureLayer.on('layeradd', function(e) {
+			    var marker = e.layer,
+			        feature = marker.feature;
+			    marker.setIcon(L.icon(feature.properties.icon));
+			});
+
+			map.setView([1.3000, 103.8000], 16);
+			getLocation(map, featureLayer);
 			// map.setView([meCurrentLatitude, meCurrentLongitude], 16);
 		}	
 	});
 })
 
-function loadMap(position, map){
+function loadMap(position, map, featureLayer){
 	var meLayer = null;
 	var meCurrentLatitude = position.coords.latitude;
 	var meCurrentLongitude =  position.coords.longitude;
@@ -44,12 +53,11 @@ function loadMap(position, map){
 		if (Mapbox.loaded()) {
 			console.log(map);
 			if (map != null){
-				map.remove();
-				map = L.mapbox.map("map", "mapbox.emerald");
-				map.setView([meCurrentLatitude, meCurrentLongitude], 16);
+			// 	map.remove();
+			// 	map = L.mapbox.map("map", "mapbox.emerald");
+				map.setView([meCurrentLatitude, meCurrentLongitude], map._zoom);
 			}
 			// Dynamic Add and Remove Points
-			featureLayer = L.mapbox.featureLayer().addTo(map);
 			var geoJson = [
 				{
 			        type: "Feature",
@@ -71,20 +79,17 @@ function loadMap(position, map){
 		    ];
 
 		    // Set a custom icon on each marker based on feature properties.
-			featureLayer.on('layeradd', function(e) {
-			    var marker = e.layer,
-			        feature = marker.feature;
+			
+			featureLayer.setGeoJSON([]);
 
-			    marker.setIcon(L.icon(feature.properties.icon));
-			});
 			featureLayer.setGeoJSON(geoJson);
 
 		}
 	// });
-	var loop = setTimeout(getLocation, 5000, map);
+	var loop = setTimeout(getLocation, 5000, map, featureLayer);
 }
 
-function getLocation(map){
+function getLocation(map, featureLayer){
 	console.log("!!!!!!!!!!")
 	var locationFailure = false;
 	// var currentLocation = { coords: { latitude: 1.296750, longitude: 103.773186 } };
@@ -92,7 +97,7 @@ function getLocation(map){
 		navigator.geolocation.getCurrentPosition(function (position) {  
 			if (position.coords && position.coords.latitude && position.coords.longitude) {
 				// XYZ
-				loadMap(position, map);
+				loadMap(position, map, featureLayer);
 				console.log('maploaded');
 			} else {
 				locationFailure = true;
