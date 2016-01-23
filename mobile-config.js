cc9e201a190efc03fdf437dea3cadd68fcfc05f0
Zheng Yang Tean <teanzhengyang@outlook.sg>
2016-01-24 02:14:20 +0800

    app info

--- mobile-config.js
+++ mobile-config.js
@@ -3,3 +3,10 @@ App.configurePlugin('com.phonegap.plugins.facebookconnect', {
     APP_ID: '446286968895184',
     APP_NAME: 'Bump-Into'
 });
+
+App.info({
+  name: 'Bump-Into',
+  description: 'A Hack&Roll Project',
+  version: '0.0.1'
+});
+
