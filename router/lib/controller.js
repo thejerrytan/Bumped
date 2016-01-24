d88df580f4607e886976fc06c44617c42c1ef879
Ang Hao Yang <yan9za1@gmail.com>
2016-01-24 12:42:20 +0800

    Fix

--- router/lib/controller.js
+++ router/lib/controller.js
@@ -1,7 +1,7 @@
 LoginController = RouteController.extend({
     onBeforeAction: function () {
-        //if(!Meteor.user()) return this.render('landingpage');
-        if(!Meteor.user()) return Router.go('landingpage');
+        if(!Meteor.user()) return this.render('landingpage');
+        //if(!Meteor.user()) return Router.go('landingpage');
 
         this.next();
     }
